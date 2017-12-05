b943fbc053336c6270c5c68c3786018e531eaf64
brixton <brixton.mavu@rockectseed.com>
2017-12-05 23:35:54 +0200

    added a slideshow

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,7 @@
-// var slides = document.querySelectorAll('#slides .slide');
-// var currentSlide = 0;
-// var slideInterval = setInterval(nextSlide,2000);
-
-// function nextSlide(){
-// 	slides[currentSlide].className = 'slide';
-// 	currentSlide = (currentSlide+1)%slides.length;
-// 	slides[currentSlide].className = 'slide showing';
-// }
+$(function() {
+    $('body').chocolate({
+        images		: ['/images/EPA1.jpg', '/images/EPA2.jpg','/images/EPA3.jpg','/images/EPA4.jpg', '/images/EPA5.jpg','/images/EPA6.jpg','/images/EPA7.jpg', '/images/EPA8.jpg','/images/EPA9.jpg','/images/EPA10.jpg','/images/EPA11.jpg'],
+        interval	: 4000,
+        speed		: 2000,
+    });
+});
